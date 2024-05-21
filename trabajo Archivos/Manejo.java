eb02cec2fe16951b0f185b6cb6095ea662aa4dce
teo2611 <mateosepulveda26u@gmail.com>
2024-05-21 10:44:13 -0500

    BorradoComentarios

--- trabajo Archivos/Manejo.java
+++ trabajo Archivos/Manejo.java
@@ -3,10 +3,7 @@ public class Manejo {
     public static void main(String[] args) throws IOException {
         Menu menu = new Menu();
         menu.menu();
-        System.out.println("Hola");
-        System.out.println("Hola");
-        System.out.println("Hola");
-        System.out.println("ksksks");
+
         
     }
 }
